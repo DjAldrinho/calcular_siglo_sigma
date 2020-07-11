cda321e88b130e48c55a316935e9c9b5cc77a515
Dj Aldrinho <aldray.devsoft@gmail.com>
2020-07-11 14:34:20 -0500

    end

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Calcular Siglo</title>
+</head>
+<body>
+<form action="procesar.php" method="post">
+    <h1>Calcular Siglo</h1>
+    <p>Ingrese un año (Debe ser entero)</p>
+    <input type="number" name="year">
+    <button type="submit">Enviar</button>
+</form>
+</body>
+</html>
--- procesar.php
+++ procesar.php
@@ -1,1 +1,30 @@
 <?php
+
+$mensaje_validacion = '';
+if (isset($_POST) && count($_POST) > 0){
+
+    $year = (int)$_POST['year'];
+    $siglo  = 0;
+
+    if($year >= 1 && is_numeric($year)){
+
+        if($year <= 100){
+            $siglo = 1;
+        } elseif ($year % 100 === 0){
+            $siglo = $year / 100;
+        } else {
+            $siglo = floor($year / 100) + 1;
+        }
+
+        $mensaje_validacion =  "El año ingresado es: $year y pertenece al siglo $siglo";
+    } else {
+        $mensaje_validacion =  'Ingrese un año valido!';
+    }
+
+
+    echo <<<HTML
+    <p>$mensaje_validacion</p>
+    <p><a href="index.php">back</a> Regresar</p>
+HTML;
+
+}
